b16e9e2a7614d8e2bf8d6ebf73645f73ca5a9151
Sherwyn Poon <sherwyn@spoon.pm>
2016-12-15 00:23:42 -0500

    ~map view

--- js/map.js
+++ js/map.js
@@ -6,8 +6,22 @@ $(function() {
   // Set the neighborhood label.
   $("#neighborhood-label").text(address);
   
+  // Set the neighborhood label.
+  $("#crime-trends").text("Crime Trends in " + address);
+  
+  // Set the neighborhood label.
+  $("#crime-reports").text("Crime Reports in " + address);
+  
+  // Set the date label.
+  $("#date-range").text("Nov. 2 - Dec. 2, 2016");
+  
   $("form").submit(function(e) {
     var address = $("#address").val();
+    
+    // If no address, prevent form submit
+    if (!address) {
+      e.preventDefault()
+    }
     
     // Save input to localStorage. We'll access this later in map.js.
     localStorage.setItem("address", address);
--- js/search.js
+++ js/search.js
@@ -3,6 +3,11 @@ $(function() {
   $("form").submit(function(e) {
     var address = $("#address").val();
     
+    // If no address, prevent form submit
+    if (!address) {
+      e.preventDefault()
+    }
+    
     // Save input to localStorage. We'll access this later in map.js.
     localStorage.setItem("address", address);
   })
