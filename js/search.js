fff1c8ddfecd975be986b2637ef0dae3cdad4a4f
ssyssy <ssyssy@umich.edu>
2016-12-15 19:07:33 -0500

    google map api added

--- js/search.js
+++ js/search.js
@@ -6,9 +6,23 @@ $(function() {
     // If no address, prevent form submit
     if (!address) {
       e.preventDefault()
+      return
     }
-    
+    var new_address = address.split(' ').join('+');
     // Save input to localStorage. We'll access this later in map.js.
-    localStorage.setItem("address", address);
+    var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + new_address+",+Detroit";
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false ); 
+    xmlHttp.send();
+    var str = xmlHttp.responseText;
+
+    var result = jQuery.parseJSON(str);
+    if (result.status =="ZERO_RESULTS") {
+      e.preventDefault()
+      return
+    }
+    console.log(result.results.length);
+    localStorage.setItem("address", result.results[0].formatted_address);
+    localStorage.setItem("coordinates", result.results[0].geometry.location);
   })
 })
